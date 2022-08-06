12abd54e2e66f58abb9184503037fcc4fb674997
Aleksandr <aleksandr.kiliushin@gmail.com>
2022-08-06 14:21:35 +0400

    n49: test: responds with a finance category type for a given id

--- e2e/finances/finance-category-types/index.test.ts
+++ e2e/finances/finance-category-types/index.test.ts
@@ -12,4 +12,12 @@ describe("finance-category-types", () => {
       { id: 2, name: "income" },
     ])
   })
+
+  it("responds with a finance category type for a given id", async () => {
+    const authToken = await logIn({ username: "john-doe", password: "john-doe-password" })
+    const getFinanceCategoryTypeWithIdOf2 = await fetch("http://localhost:3080/api/finances/category-type/2", {
+      headers: { Authorization: "Bearer " + authToken },
+    })
+    expect(await getFinanceCategoryTypeWithIdOf2.json()).toEqual<IFinanceCategoryType>({ id: 2, name: "income" })
+  })
 })
