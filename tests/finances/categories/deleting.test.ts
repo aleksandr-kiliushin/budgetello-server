a628d9967432d45f7ff005b3b70457d3dbdc7c2e
Aleksandr <aleksandr.kiliushin@gmail.com>
2022-08-09 23:49:30 +0400

    test: Uncomment finance category deleting

--- tests/finances/categories/deleting.test.ts
+++ tests/finances/categories/deleting.test.ts
@@ -1,43 +1,38 @@
-// import { IFinanceCategory } from "../../../src/interfaces/finance"
-// import { logIn } from "../../helpers/logIn"
+import { IFinanceCategory } from "../../../src/interfaces/finance"
+import { logIn } from "../../helpers/logIn"
 
-// let authToken = ""
-// beforeEach(async () => {
-//   authToken = await logIn({ username: "john-doe", password: "john-doe-password" })
-// })
+let authToken = ""
+beforeEach(async () => {
+  authToken = await logIn({ username: "john-doe", password: "john-doe-password" })
+})
 
 describe("Finance category deleting", () => {
-  // TODO: Uncomment tests after DB with new cascade settings will be available on remote server.
-  it("dummy", () => {
-    expect(true).toEqual(true)
+  it("returns a correct response after deleting", async () => {
+    const categoryCreatingResponse = await fetch("http://localhost:3080/api/finances/categories/2", {
+      headers: { Authorization: authToken },
+      method: "DELETE",
+    })
+    expect(categoryCreatingResponse.status).toEqual(200)
+    expect(await categoryCreatingResponse.json()).toEqual<IFinanceCategory>({
+      id: 2,
+      name: "education",
+      type: { id: 1, name: "expense" },
+    })
   })
 
-  // it("returns a correct response after deleting", async () => {
-  //   const categoryCreatingResponse = await fetch("http://localhost:3080/api/finances/categories/2", {
-  //     headers: { Authorization: authToken },
-  //     method: "DELETE",
-  //   })
-  //   expect(categoryCreatingResponse.status).toEqual(200)
-  //   expect(await categoryCreatingResponse.json()).toEqual<IFinanceCategory>({
-  //     id: 2,
-  //     name: "education",
-  //     type: { id: 1, name: "expense" },
-  //   })
-  // })
-
-  // it("the deleted category is not presented in all categories list", async () => {
-  //   await fetch("http://localhost:3080/api/finances/categories/2", {
-  //     headers: { Authorization: authToken },
-  //     method: "DELETE",
-  //   })
-  //   const getAllCategoriesResponse = await fetch("http://localhost:3080/api/finances/categories/search", {
-  //     headers: { Authorization: authToken },
-  //   })
-  //   expect(await getAllCategoriesResponse.json()).toEqual<IFinanceCategory[]>([
-  //     { id: 1, name: "clothes", type: { id: 1, name: "expense" } },
-  //     { id: 3, name: "gifts", type: { id: 1, name: "expense" } },
-  //     { id: 4, name: "gifts", type: { id: 2, name: "income" } },
-  //     { id: 5, name: "salary", type: { id: 2, name: "income" } },
-  //   ])
-  // })
+  it("the deleted category is not presented in all categories list", async () => {
+    await fetch("http://localhost:3080/api/finances/categories/2", {
+      headers: { Authorization: authToken },
+      method: "DELETE",
+    })
+    const getAllCategoriesResponse = await fetch("http://localhost:3080/api/finances/categories/search", {
+      headers: { Authorization: authToken },
+    })
+    expect(await getAllCategoriesResponse.json()).toEqual<IFinanceCategory[]>([
+      { id: 1, name: "clothes", type: { id: 1, name: "expense" } },
+      { id: 3, name: "gifts", type: { id: 1, name: "expense" } },
+      { id: 4, name: "gifts", type: { id: 2, name: "income" } },
+      { id: 5, name: "salary", type: { id: 2, name: "income" } },
+    ])
+  })
 })
