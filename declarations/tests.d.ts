f671d3fa58602de8bddc3ad0cf788777f1bedbea
Aleksandr <aleksandr.kiliushin@gmail.com>
2022-08-10 10:40:04 +0400

    chore: Configure global variables for tests

---
+++ declarations/tests.d.ts
@@ -0,0 +1,5 @@
+declare global {
+  const authToken: string
+}
+
+export {}
--- jest.setup.ts
+++ jest.setup.ts
@@ -4,6 +4,10 @@ beforeEach(async () => {
   execSync('echo "bash /var/app/restore-db-from-testing-template.sh" | docker exec -i personal-app-database bash;')
 })
 
+afterEach(() => {
+  globalThis.authToken = ""
+})
+
 afterAll(() => {
   execSync('echo "bash /var/app/restore-db-from-dev-template.sh" | docker exec -i personal-app-database bash;')
 })
