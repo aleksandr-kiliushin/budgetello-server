0b34f4a9d3f2ccf256597ff47e626d3a377d5032
Aleksandr <aleksandr.kiliushin@gmail.com>
2022-08-14 20:54:28 +0400

    check if API_PORT is defined

--- src/main.ts
+++ src/main.ts
@@ -3,10 +3,14 @@ import { NestFactory } from "@nestjs/core"
 import { AppModule } from "src/app.module"
 
 async function bootstrap() {
+  if (process.env.PORT === undefined) {
+    throw new Error("process.env.PORT is undefined.")
+  }
+
   const app = await NestFactory.create(AppModule)
   app.enableCors()
   app.setGlobalPrefix("api")
-  await app.listen(process.env.PORT || 3080)
+  await app.listen(process.env.PORT)
 }
 
 bootstrap()
