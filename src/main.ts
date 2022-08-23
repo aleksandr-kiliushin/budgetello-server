3f225b216c0d76c46ccc35714b0040df843960dd
Aleksandr <aleksandr.kiliushin@gmail.com>
2022-08-23 15:03:28 +0400

    Rename API_PORT with PORT

--- src/main.ts
+++ src/main.ts
@@ -3,14 +3,14 @@ import { NestFactory } from "@nestjs/core"
 import { AppModule } from "src/app.module"
 
 async function bootstrap() {
-  if (process.env.API_PORT === undefined) {
-    throw new Error("process.env.API_PORT is undefined.")
+  if (process.env.PORT === undefined) {
+    throw new Error("process.env.PORT is undefined.")
   }
 
   const app = await NestFactory.create(AppModule)
   app.enableCors()
   app.setGlobalPrefix("api")
-  await app.listen(process.env.API_PORT)
+  await app.listen(process.env.PORT)
 }
 
 bootstrap()
