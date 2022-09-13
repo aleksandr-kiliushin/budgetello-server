b43f45129cc6e261f931647f9fdc309643aa849c
Aleksandr <aleksandr.kiliushin@gmail.com>
2022-09-13 17:08:09 +0400

    n51: test: Refactor tests

--- e2e/finances/categories/updating.test.ts
+++ e2e/finances/categories/updating.test.ts
@@ -7,19 +7,6 @@ beforeEach(async () => {
 })
 
 describe("Finance category updating", () => {
-  it("returns a correct response after updating", async () => {
-    const categoryUpdatingResponse = await fetchApi("/api/finances/categories/2", {
-      body: JSON.stringify({ name: "teaching", typeId: 2 }),
-      method: "PATCH",
-    })
-    expect(categoryUpdatingResponse.status).toEqual(200)
-    expect(await categoryUpdatingResponse.json()).toEqual<IFinanceCategory>({
-      id: 2,
-      name: "teaching",
-      type: { id: 2, name: "income" },
-    })
-  })
-
   it("updated category is presented in all categories list", async () => {
     await fetchApi("/api/finances/categories/2", { body: JSON.stringify({ name: "drugs" }), method: "PATCH" })
     const getAllCategoriesResponse = await fetchApi("/api/finances/categories/search")
@@ -38,5 +25,56 @@ describe("Finance category updating", () => {
       name: "education",
       type: { id: 2, name: "income" },
     })
+  })
+
+  test.each<{
+    payload: Record<string, string | number>
+    response: Record<string, unknown>
+    status: number
+    url: string
+  }>([
+    // {
+    //   payload: { name: "", typeId: 1 },
+    //   response: { fields: { name: "Required field." } },
+    //   status: 400,
+    // },
+    // {
+    //   payload: { name: "food", typeId_WITH_A_TYPO: 1 },
+    //   response: { fields: { typeId: "Required field." } },
+    //   status: 400,
+    // },
+    // {
+    //   payload: { name: "food", typeId: 1234123 },
+    //   response: { fields: { typeId: "Invalid category type." } },
+    //   status: 400,
+    // },
+    // {
+    //   payload: { name: "education", typeId: 1 },
+    //   response: {
+    //     fields: {
+    //       name: '"education" expense category already exists.',
+    //       typeId: '"education" expense category already exists.',
+    //     },
+    //   },
+    //   status: 400,
+    // },
+    // {
+    //   payload: { name: "education", typeId: 2 },
+    //   response: { id: 6, name: "education", type: { id: 2, name: "income" } },
+    //   status: 201,
+    // },
+    {
+      payload: { name: "teaching", typeId: 2 },
+      response: { id: 2, name: "teaching", type: { id: 2, name: "income" } },
+      status: 200,
+      url: "/api/finances/categories/2",
+    },
+  ])("Category editing case #%#", async ({ payload, response, status, url }) => {
+    const categoryUpdatingResponse = await fetchApi(url, {
+      body: JSON.stringify(payload),
+      method: "PATCH",
+    })
+    expect(categoryUpdatingResponse.status).toEqual(status)
+    expect(await categoryUpdatingResponse.json()).toEqual(response)
   })
 })
