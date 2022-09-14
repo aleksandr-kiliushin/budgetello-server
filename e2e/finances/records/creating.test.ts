ee24d86b262289e46e12d173b4ac06247ee09fd1
Aleksandr <aleksandr.kiliushin@gmail.com>
2022-09-14 14:15:44 +0400

    n51: test: Refactor and prepare finance records creating tests

--- e2e/finances/records/creating.test.ts
+++ e2e/finances/records/creating.test.ts
@@ -7,21 +7,6 @@ beforeEach(async () => {
 })
 
 describe("Finance record creating", () => {
-  it("returns a correct response after creating", async () => {
-    const recordCreatingResponse = await fetchApi("/api/finances/records", {
-      body: JSON.stringify({ amount: 2000, categoryId: 5, date: "2022-08-05" }),
-      method: "POST",
-    })
-    expect(recordCreatingResponse.status).toEqual(201)
-    expect(await recordCreatingResponse.json()).toEqual<IFinanceRecord>({
-      amount: 2000,
-      category: { id: 5, name: "salary", type: { id: 2, name: "income" } },
-      date: "2022-08-05",
-      id: 7,
-      isTrashed: false,
-    })
-  })
-
   it("a newly created record is presented in all records list", async () => {
     await fetchApi("/api/finances/records", {
       body: JSON.stringify({ amount: 2000, categoryId: 5, date: "2022-08-05" }),
@@ -37,18 +22,28 @@ describe("Finance record creating", () => {
     })
   })
 
-  it("a newly created record can be found by ID", async () => {
-    await fetchApi("/api/finances/records", {
-      body: JSON.stringify({ amount: 2000, categoryId: 5, date: "2022-08-05" }),
+  test.each<{
+    payload: Record<string, string | number>
+    response: Record<string, unknown>
+    status: number
+  }>([
+    {
+      payload: { amount: 2000, categoryId: 5, date: "2022-08-05" },
+      response: {
+        amount: 2000,
+        category: { id: 5, name: "salary", type: { id: 2, name: "income" } },
+        date: "2022-08-05",
+        id: 7,
+        isTrashed: false,
+      },
+      status: 201,
+    },
+  ])("Finance record creating case #%#", async ({ payload, response, status }) => {
+    const recordCreatingResponse = await fetchApi("/api/finances/records", {
+      body: JSON.stringify(payload),
       method: "POST",
     })
-    const getNewlyCreatedRecordResponse = await fetchApi("/api/finances/records/7")
-    expect(await getNewlyCreatedRecordResponse.json()).toEqual<IFinanceRecord>({
-      amount: 2000,
-      category: { id: 5, name: "salary", type: { id: 2, name: "income" } },
-      date: "2022-08-05",
-      id: 7,
-      isTrashed: false,
-    })
+    expect(recordCreatingResponse.status).toEqual(status)
+    expect(await recordCreatingResponse.json()).toEqual(response)
   })
 })
