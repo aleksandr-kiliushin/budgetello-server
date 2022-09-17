acca5c0f086bb8d7e0a82f5ce7e42b818c0e437b
Aleksandr <aleksandr.kiliushin@gmail.com>
2022-09-17 11:59:17 +0400

    n53: refactor: Reorder eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,10 @@
 module.exports = {
+  env: {
+    node: true,
+    jest: true,
+  },
+  extends: ["plugin:@typescript-eslint/recommended", "plugin:prettier/recommended"],
+  ignorePatterns: [".eslintrc.js"],
   parser: "@typescript-eslint/parser",
   parserOptions: {
     project: "tsconfig.eslint.json",
@@ -6,13 +12,7 @@ module.exports = {
     tsconfigRootDir: __dirname,
   },
   plugins: ["@typescript-eslint/eslint-plugin"],
-  extends: ["plugin:@typescript-eslint/recommended", "plugin:prettier/recommended"],
   root: true,
-  env: {
-    node: true,
-    jest: true,
-  },
-  ignorePatterns: [".eslintrc.js"],
   rules: {
     "@typescript-eslint/interface-name-prefix": "off",
     "@typescript-eslint/explicit-module-boundary-types": "off",
