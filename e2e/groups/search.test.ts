65e8bbc17ca086b7b5cd7f295f113938ac6d6e0c
Aleksandr <aleksandr.kiliushin@gmail.com>
2022-09-20 20:24:04 +0400

    n55: test: Add groups search tests

--- e2e/groups/search.test.ts
+++ e2e/groups/search.test.ts
@@ -66,24 +66,30 @@ describe("Finance categoires search", () => {
         },
       ],
     },
-    // {
-    //   url: "/api/finances/categories/search?id=666666",
-    //   searchResult: [],
-    // },
-    // {
-    //   url: "/api/finances/categories/search?id=66666,5",
-    //   searchResult: [{ id: 5, name: "salary", type: { id: 2, name: "income" } }],
-    // },
-    // {
-    //   url: "/api/finances/categories/search",
-    //   searchResult: [
-    //     { id: 1, name: "clothes", type: { id: 1, name: "expense" } },
-    //     { id: 2, name: "education", type: { id: 1, name: "expense" } },
-    //     { id: 3, name: "gifts", type: { id: 1, name: "expense" } },
-    //     { id: 4, name: "gifts", type: { id: 2, name: "income" } },
-    //     { id: 5, name: "salary", type: { id: 2, name: "income" } },
-    //   ],
-    // },
+    {
+      url: "/api/groups/search?id=666666",
+      searchResult: [],
+    },
+    {
+      url: "/api/groups/search",
+      searchResult: [
+        {
+          id: 1,
+          name: "clever-financiers",
+          subject: { id: 1, name: "finances" },
+          users: [{ id: 1, username: "john-doe", password: "8bd309ffba83c3db9a53142b052468007b" }],
+        },
+        {
+          id: 2,
+          name: "beautiful-sportsmen",
+          subject: { id: 2, name: "habits" },
+          users: [
+            { id: 1, username: "john-doe", password: "8bd309ffba83c3db9a53142b052468007b" },
+            { id: 2, username: "jessica-stark", password: "8bd912e2fe84cd93c457142a1d7e77136c3bc954f183" },
+          ],
+        },
+      ],
+    },
   ])("categories search for: $url", async ({ url, searchResult }) => {
     const response = await fetchApi(url)
     expect(response.status).toEqual(200)
