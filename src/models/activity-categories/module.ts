683a11b099d2777e5258ce43c1ff4878d89a712e
Aleksandr <aleksandr.kiliushin@gmail.com>
2022-10-09 14:00:29 +0400

    n64: feat: Add ActivityCategoryMeasurementTypesModule to ActivityCategoresModule imports

--- src/models/activity-categories/module.ts
+++ src/models/activity-categories/module.ts
@@ -1,6 +1,7 @@
 import { Module } from "@nestjs/common"
 import { TypeOrmModule } from "@nestjs/typeorm"
 
+import { ActivityCategoryMeasurementTypesModule } from "#models/activity-category-measurement-types/module"
 import { BoardsModule } from "#models/boards/module"
 import { UserModule } from "#models/user/module"
 
@@ -11,7 +12,12 @@ import { ActivityCategoriesService } from "./service"
 @Module({
   controllers: [ActivityCategoriesController],
   exports: [ActivityCategoriesService],
-  imports: [TypeOrmModule.forFeature([ActivityCategoryEntity]), BoardsModule, UserModule],
+  imports: [
+    TypeOrmModule.forFeature([ActivityCategoryEntity]),
+    ActivityCategoryMeasurementTypesModule,
+    BoardsModule,
+    UserModule,
+  ],
   providers: [ActivityCategoriesController, ActivityCategoriesService],
 })
 export class ActivityCategoriesModule {}
