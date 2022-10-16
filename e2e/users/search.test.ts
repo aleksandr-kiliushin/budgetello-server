0a59fcd14437fc1cb55376eae9d2260f198dc9f4
Aleksandr <aleksandr.kiliushin@gmail.com>
2022-10-16 11:52:29 +0400

    n71: test: Restore users search

--- e2e/users/search.test.ts
+++ e2e/users/search.test.ts
@@ -1,59 +1,148 @@
-import { IUser } from "#interfaces/user"
-
 import { users } from "#e2e/constants/users"
 import { authorize } from "#e2e/helpers/authorize"
-import { fetchApi } from "#e2e/helpers/fetchApi"
+import { fetchGqlApi } from "#e2e/helpers/fetchGqlApi"
 
 beforeEach(async () => {
   await authorize(users.johnDoe.username)
 })
 
 describe("Returns a user by their identifier", () => {
-  test.each<
-    | {
-        url: string
-        responseStatus: 200
-        responseData: IUser
-      }
-    | {
-        url: string
-        responseStatus: 404
-        responseData: Record<string, never>
-      }
-  >([
-    { url: "/api/users/0", responseStatus: 200, responseData: users.johnDoe },
-    { url: "/api/users/1", responseStatus: 200, responseData: users.johnDoe },
-    { url: "/api/users/john-doe", responseStatus: 200, responseData: users.johnDoe },
-    { url: "/api/users/john", responseStatus: 404, responseData: {} },
-    { url: "/api/users/nonexistent-username", responseStatus: 404, responseData: {} },
-    { url: "/api/users/123456789", responseStatus: 404, responseData: {} },
-  ])("user: $url", async ({ url, responseStatus, responseData }) => {
-    const response = await fetchApi(url)
-    expect(response.status).toEqual(responseStatus)
-    expect(await response.json()).toEqual(responseData)
+  test.each<{ query: string; responseBody: unknown }>([
+    {
+      query: `{
+        user(id: 0) {
+          id, password, username
+        }
+      }`,
+      responseBody: { data: { user: users.johnDoe } },
+    },
+    {
+      query: `{
+        user(id: 1) {
+          id, password, username
+        }
+      }`,
+      responseBody: { data: { user: users.johnDoe } },
+    },
+    {
+      query: `{
+        user(username: "john-doe") {
+          id, password, username
+        }
+      }`,
+      responseBody: { data: { user: users.johnDoe } },
+    },
+    // {
+    //   url: "/api/users/123456789",
+    //   responseStatus: 404,
+    //   responseBody: {},
+    // },
+    // {
+    //   url: "/api/users/john",
+    //   responseStatus: 404,
+    //   responseBody: {},
+    // },
+    // {
+    //   url: "/api/users/nonexistent-username",
+    //   responseStatus: 404,
+    //   responseBody: {},
+    // },
+  ])("$url", async ({ query, responseBody }) => {
+    expect(await fetchGqlApi(query)).toEqual(responseBody)
   })
 })
 
 describe("Users search", () => {
-  test.each<{ url: string; responseBody: unknown; responseStatus: number }>([
-    { url: "/api/users/search?ids=1", responseBody: [users.johnDoe], responseStatus: 200 },
-    { url: "/api/users/search?ids=1,2", responseBody: [users.johnDoe, users.jessicaStark], responseStatus: 200 },
-    { url: "/api/users/search?username=john-doe", responseBody: [users.johnDoe], responseStatus: 200 },
-    { url: "/api/users/search?ids=1&username=john-doe", responseBody: [users.johnDoe], responseStatus: 200 },
-    { url: "/api/users/search?username=john", responseBody: [users.johnDoe], responseStatus: 200 },
-    { url: "/api/users/search?username=doe", responseBody: [users.johnDoe], responseStatus: 200 },
-    { url: "/api/users/search?username=j", responseBody: [users.johnDoe, users.jessicaStark], responseStatus: 200 },
-    { url: "/api/users/search?username=nonexistent-username", responseBody: [], responseStatus: 200 },
-    { url: "/api/users/search?ids=666666", responseBody: [], responseStatus: 200 },
-    { url: "/api/users/search", responseBody: [users.johnDoe, users.jessicaStark], responseStatus: 200 },
+  test.each<{ query: string; responseBody: unknown }>([
     {
-      url: "/api/users/search?ids=1,hello",
-      responseBody: { query: { ids: "An array of numbers expected." } },
-      responseStatus: 400,
+      query: `{
+        users(ids: [1]) {
+          id,
+          password,
+          username
+        }
+      }`,
+      responseBody: { data: { users: [users.johnDoe] } },
     },
-  ])("case: $url", async ({ url, responseBody, responseStatus }) => {
-    const response = await fetchApi(url)
-    expect(response.status).toEqual(responseStatus)
-    expect(await response.json()).toEqual(responseBody)
+    {
+      query: `{
+        users(ids: [1, 2]) {
+          id,
+          password,
+          username
+        }
+      }`,
+      responseBody: { data: { users: [users.johnDoe, users.jessicaStark] } },
+    },
+    {
+      query: `{
+        users(username: "john-doe") {
+          id,
+          password,
+          username
+        }
+      }`,
+      responseBody: { data: { users: [users.johnDoe] } },
+    },
+    {
+      query: `{
+        users(ids: [1], username: "john-doe") {
+          id,
+          password,
+          username
+        }
+      }`,
+      responseBody: { data: { users: [users.johnDoe] } },
+    },
+    {
+      query: `{
+        users(username: "john") {
+          id,
+          password,
+          username
+        }
+      }`,
+      responseBody: { data: { users: [users.johnDoe] } },
+    },
+    {
+      query: `{
+        users(username: "doe") {
+          id,
+          password,
+          username
+        }
+      }`,
+      responseBody: { data: { users: [users.johnDoe] } },
+    },
+    {
+      query: `{
+        users(username: "j") {
+          id,
+          password,
+          username
+        }
+      }`,
+      responseBody: { data: { users: [users.johnDoe, users.jessicaStark] } },
+    },
+    {
+      query: `{
+        users {
+          id,
+          password,
+          username
+        }
+      }`,
+      responseBody: { data: { users: [users.johnDoe, users.jessicaStark] } },
+    },
+    // { url: "/api/users/search?username=nonexistent-username", responseBody: [], responseStatus: 200 },
+    // { url: "/api/users/search?ids=666666", responseBody: [], responseStatus: 200 },
+    // { url: "/api/users/search", responseBody: [users.johnDoe, users.jessicaStark], responseStatus: 200 },
+    // {
+    //   url: "/api/users/search?ids=1,hello",
+    //   responseBody: { query: { ids: "An array of numbers expected." } },
+    //   responseStatus: 400,
+    // },
+  ])("$url", async ({ query, responseBody }) => {
+    expect(await fetchGqlApi(query)).toEqual(responseBody)
   })
 })
