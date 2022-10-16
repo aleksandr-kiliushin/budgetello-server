ba0a4d359baed8b56b3c124070edda967471b669
Aleksandr <aleksandr.kiliushin@gmail.com>
2022-10-16 22:59:53 +0400

    n71: test: Fix user updating tests

--- e2e/users/updating.test.ts
+++ e2e/users/updating.test.ts
@@ -1,83 +1,79 @@
 import { users } from "#e2e/constants/users"
 import { authorize } from "#e2e/helpers/authorize"
 import { fetchGqlApi } from "#e2e/helpers/fetchGqlApi"
+import { pickFields } from "#e2e/pickFields"
 
 beforeEach(async () => {
   await authorize(users.johnDoe.id)
 })
 
 describe("User updating", () => {
-  it("allows the logged in user to update themselves", async () => {
+  it("allows the authorized user to update themselves", async () => {
     const responseBody = await fetchGqlApi(`mutation UPDATE_USER {
       updateUser(input: { id: ${users.johnDoe.id}, username: "john-doe-is-cool", password: "john-doe-new-password" }) {
-        id,
-        password,
-        username
+        ${pickFields.user}
       }
     }`)
-    expect(responseBody).toEqual({
-      data: {
-        updateUser: {
-          id: users.johnDoe.id,
-          username: "john-doe-is-cool",
-          password: expect.stringMatching(".+"),
-        },
+    expect(responseBody.data).toEqual({
+      updateUser: {
+        id: users.johnDoe.id,
+        username: "john-doe-is-cool",
+        password: expect.stringMatching(".+"),
       },
     })
   })
 
-  it("user can authorize with the new credentials", async () => {
+  it("authorization with the new credential is successful", async () => {
     await fetchGqlApi(`mutation UPDATE_USER {
       updateUser(input: { id: ${users.johnDoe.id}, username: "john-doe-is-cool", password: "john-doe-new-password" }) {
-        id,
-        password,
-        username
+        ${pickFields.user}
       }
     }`)
-    const authorizeWithNewCredentialsResponse = await fetch("http://localhost:3080/api/authorize", {
-      body: JSON.stringify({ username: "john-doe-is-cool", password: "john-doe-new-password" }),
-      headers: { "Content-Type": "application/json" },
+    const response = await fetch("http://localhost:3080/graphql", {
+      body: JSON.stringify({
+        query: `mutation AUTHORIZE {
+          authorize(input: { username: "john-doe-is-cool", password: "john-doe-new-password" })
+        }`,
+      }),
+      headers: { Accept: "application/json", "Content-Type": "application/json" },
       method: "POST",
     })
-    expect(authorizeWithNewCredentialsResponse.status).toEqual(201)
-    expect(await authorizeWithNewCredentialsResponse.json()).toEqual({
-      authorizationToken: expect.stringMatching(".+"),
-    })
+    expect(response.status).toEqual(200)
+    const responseBody = await response.json()
+    expect(responseBody.data.authorize).toEqual(expect.stringMatching(".+"))
   })
 
-  it("doesn't allow update another user", async () => {
-    const responseBody = await fetchGqlApi(`mutation UPDATE_USER {
-      updateUser(input: { id: ${users.jessicaStark.id}, username: "jessica-stark-is-stupid" }) {
-        id,
-        password,
-        username
+  it("authorization with the old credential is failed", async () => {
+    await fetchGqlApi(`mutation UPDATE_USER {
+      updateUser(input: { id: ${users.johnDoe.id}, username: "john-doe-is-cool", password: "john-doe-new-password" }) {
+        ${pickFields.user}
       }
     }`)
-    expect(responseBody.errors[0].extensions.exception.response).toEqual({ message: "Access denied." })
+    const response = await fetch("http://localhost:3080/graphql", {
+      body: JSON.stringify({
+        query: `mutation AUTHORIZE {
+          authorize(input: { username: "john-doe", password: "john-doe-password" })
+        }`,
+      }),
+      headers: { Accept: "application/json", "Content-Type": "application/json" },
+      method: "POST",
+    })
+    const responseBody = await response.json()
+    expect(responseBody.errors[0].extensions.exception.response).toEqual({ fields: { username: "User not found." } })
+  })
+
+  it("updating another user is failed", async () => {
+    const updateAnotherUserResponseBody = await fetchGqlApi(`mutation UPDATE_USER {
+      updateUser(input: { id: ${users.jessicaStark.id}, username: "john-doe-is-cool", password: "john-doe-new-password" }) {
+        ${pickFields.user}
+      }
+    }`)
+    expect(updateAnotherUserResponseBody.errors[0].extensions.exception.response).toEqual({ message: "Access denied." })
     const fetchAnotherUserResponseBody = await fetchGqlApi(`{
       user(id: ${users.jessicaStark.id}) {
-        id,
-        password,
-        username
+        ${pickFields.user}
       }
     }`)
-    expect(fetchAnotherUserResponseBody).toEqual({ data: { user: users.jessicaStark } })
-  })
-
-  it("user cannot authorize with the old credentials", async () => {
-    await fetchGqlApi(`mutation UPDATE_USER {
-      updateUser(input: { id: ${users.johnDoe.id}, username: "john-doe-is-cool", password: "john-doe-new-password" }) {
-        id,
-        password,
-        username
-      }
-    }`)
-    const authWithTheOldCredentialsResponse = await fetch("http://localhost:3080/api/authorize", {
-      body: JSON.stringify({ username: "john-doe", password: "john-doe-password" }),
-      headers: { "Content-Type": "application/json" },
-      method: "POST",
-    })
-    expect(authWithTheOldCredentialsResponse.status).toEqual(400)
-    expect(await authWithTheOldCredentialsResponse.json()).toEqual({ fields: { username: "User not found." } })
+    expect(fetchAnotherUserResponseBody.data.user).toEqual(users.jessicaStark)
   })
 })
