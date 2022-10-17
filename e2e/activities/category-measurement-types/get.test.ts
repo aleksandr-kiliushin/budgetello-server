ed1a7d0a17f74a86fc831f11e804fa6cd7540602
Aleksandr <aleksandr.kiliushin@gmail.com>
2022-10-17 15:33:29 +0400

    n71: test: Refactor category-measurement-type tests

--- e2e/activities/category-measurement-types/get.test.ts
+++ e2e/activities/category-measurement-types/get.test.ts
@@ -1,5 +1,6 @@
 import { activityCategoryMeasurementTypes } from "#e2e/constants/activities"
 import { users } from "#e2e/constants/users"
+import { QueryFields } from "#e2e/helpers/QueryFields"
 import { authorize } from "#e2e/helpers/authorize"
 import { fetchGqlApi } from "#e2e/helpers/fetchGqlApi"
 
@@ -11,31 +12,23 @@ describe("get activity category measurement types", () => {
   it("responds with all types list", async () => {
     const response = await fetchGqlApi(`{
       activityCategoryMeasurementTypes {
-        id,
-        name
+        ${QueryFields.activityCategoryMeasurementType}
       }
     }`)
-    expect(response).toEqual({
-      data: {
-        activityCategoryMeasurementTypes: [
-          activityCategoryMeasurementTypes.quantitative,
-          activityCategoryMeasurementTypes.boolean,
-        ],
-      },
+    expect(response.data).toEqual({
+      activityCategoryMeasurementTypes: [
+        activityCategoryMeasurementTypes.quantitative,
+        activityCategoryMeasurementTypes.boolean,
+      ],
     })
   })
 
   it("responds with a category type for a given id", async () => {
     const response = await fetchGqlApi(`{
       activityCategoryMeasurementType(id: ${activityCategoryMeasurementTypes.boolean.id}) {
-        id,
-        name
+        ${QueryFields.activityCategoryMeasurementType}
       }
     }`)
-    expect(response).toEqual({
-      data: {
-        activityCategoryMeasurementType: activityCategoryMeasurementTypes.boolean,
-      },
-    })
+    expect(response.data).toEqual({ activityCategoryMeasurementType: activityCategoryMeasurementTypes.boolean })
   })
 })
--- e2e/helpers/QueryFields.ts
+++ e2e/helpers/QueryFields.ts
@@ -35,4 +35,8 @@ export class QueryFields {
     id,
     isTrashed
   `
+  static activityCategoryMeasurementType = `
+    id,
+    name
+  `
 }
