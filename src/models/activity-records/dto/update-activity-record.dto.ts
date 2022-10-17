400a2484a268a30ff7bc9f22687bb316da0aed6b
Aleksandr <aleksandr.kiliushin@gmail.com>
2022-10-17 22:52:36 +0400

    n71: refactor: Remove REST DTOs from activity records

--- src/models/activity-records/dto/update-activity-record.dto.ts
+++
@@ -1,5 +0,0 @@
-import { PartialType } from "@nestjs/mapped-types"
-
-import { CreateActivityRecordDto } from "./create-activity-record.dto"
-
-export class UpdateActivityRecordDto extends PartialType(CreateActivityRecordDto) {}
