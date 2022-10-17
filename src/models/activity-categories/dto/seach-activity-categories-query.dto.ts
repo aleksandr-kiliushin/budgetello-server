e505fe9a457b890862ddb2256d7074858ad3016a
Aleksandr <aleksandr.kiliushin@gmail.com>
2022-10-17 22:40:25 +0400

    n71: refactor: Remove REST DTOs in activities

--- src/models/activity-categories/dto/seach-activity-categories-query.dto.ts
+++
@@ -1,9 +0,0 @@
-import { IActivityCategory } from "#interfaces/activities"
-import { IBoard } from "#interfaces/boards"
-import { IUser } from "#interfaces/user"
-
-export class SearchActivityCategoriesQueryDto {
-  boardsIds?: IBoard["id"][] | undefined
-  ids?: IActivityCategory["id"][] | undefined
-  ownersIds?: IUser["id"][] | undefined
-}
